223e633138d1cdae3529582dcb98251d2b87ed08
m.kindritskiy <kindritskiy.m@gmail.com>
2024-10-06 00:18:14 +0300

    Add GTest

---
+++ tests/test_message.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+
+#include "lsp.h"
+
+TEST(Message, ParseRequestMessageJson) {
+  std::string content = R"({
+    "method": "initialize",
+    "id": 1,
+    "params": {
+      "clientInfo": {
+        "name": "lets-lsp",
+        "version": "0.1.0"
+      }
+    }
+  })";
+
+  auto msg = lsp::Message::parse(content.c_str());
+  ASSERT_NE(msg, nullptr);
+  EXPECT_EQ(msg->get_kind(), lsp::Message::Kind::Request);
+
+  auto request = static_cast<lsp::RequestMessage *>(msg.get());
+  EXPECT_EQ(request->id, 1);
+  EXPECT_EQ(request->method, "initialize");
+  EXPECT_EQ(request->data["params"]["clientInfo"]["name"], "lets-lsp");
+  EXPECT_EQ(request->data["params"]["clientInfo"]["version"], "0.1.0");
+}
+
+TEST(Message, ParseNotificationMessageJson) {
+  std::string content = R"({
+    "method": "initialized",
+    "params": {}
+  })";
+
+  auto msg = lsp::Message::parse(content.c_str());
+  ASSERT_NE(msg, nullptr);
+  EXPECT_EQ(msg->get_kind(), lsp::Message::Kind::Notification);
+
+  auto request = static_cast<lsp::NotificationMessage *>(msg.get());
+  EXPECT_EQ(request->method, "initialized");
+  EXPECT_EQ(request->data["params"].size(), 0);
+}
